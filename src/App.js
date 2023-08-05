1bb2f546bd7fac9c3e734bb258305683a641a9d8
AmritSingh <amritnielit97@gmail.com>
2023-08-05 18:43:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default class App extends Component {
             progress={this}
           /> */}
           <Routes>
-            <Route exact path="/sports" element={<News apiKey={this.apiKey} key="sports " pageSize={5} country="in" category="sports" />} />
+            <Route exact path="/" element={<News apiKey={this.apiKey} key="sports " pageSize={5} country="in" category="sports" />} />
             <Route exact path="/general" element={<News apiKey={this.apiKey} key="general " pageSize={5} country="in" category="general" />} />
             <Route exact path="/health" element={<News apiKey={this.apiKey} key="health " pageSize={5} country="in" category="health" />} />
             <Route exact path="/science" element={<News apiKey={this.apiKey} key="science " pageSiz e={5} country="in" category="science" />} />
